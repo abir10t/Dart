75a250625b7e8853d9d3d4a058410f7a14a35914
abir10t <aabir.10t@gmail.com>
2021-11-26 14:04:37 +0600

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,11 @@
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main(List<String> arguments) {
+  final list4 = const [1, 2, 3];
+  final list5 = [1, 2, 3];
+
+  print(list4.hashCode);
+  print(list5.hashCode);
+  print('a');
+}
