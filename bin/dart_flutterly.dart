4d42cf2670b8ae90d16fe7827bb5434e45202e6f
abir10t <aabir.10t@gmail.com>
2021-11-28 11:41:39 +0600

    first commit

---
+++ bin/dart_flutterly.dart
@@ -0,0 +1,5 @@
+import 'package:dart_flutterly/dart_flutterly.dart' as dart_flutterly;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_flutterly.calculate()}!');
+}
---
+++ test/dart_flutterly_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_flutterly/dart_flutterly.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
