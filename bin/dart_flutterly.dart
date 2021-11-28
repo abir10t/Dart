83e2df2daced5b7901227d1fbbf1d5f5b9d8bd77
abir10t <aabir.10t@gmail.com>
2021-11-28 12:19:29 +0600

    factory constructor

--- bin/dart_flutterly.dart
+++ bin/dart_flutterly.dart
@@ -1,5 +1,33 @@
+import 'dart:math';
+
 import 'package:dart_flutterly/dart_flutterly.dart' as dart_flutterly;
 
+class Point {
+  const Point({required this.x, required this.y});
+
+  final int x;
+  final int y;
+
+  factory Point.random({required bool isPositive}) {
+    int minNegativeValue = -99;
+    int maxNegativeValue = -1;
+    int minPositiveValue = 0;
+    int maxPositiveValue = 99;
+
+    int randomMinValue = minNegativeValue +
+        Random().nextInt(maxNegativeValue - minNegativeValue);
+    Random().nextInt(maxNegativeValue - minNegativeValue);
+    return Point(x: 10, y: 20);
+  }
+
+  factory Point.exPlanation() {
+    return origin;
+  }
+
+  static const Point origin = Point(x: 0, y: 0);
+}
+
 void main(List<String> arguments) {
-  print('Hello world: ${dart_flutterly.calculate()}!');
+  var randomPositive = Point.random(isPositive: true);
+  print(randomPositive.y);
 }
